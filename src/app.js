392e02f3568dffb8eaf1d21e5e2550512baa57bc
shreha didwania <shrehadidwania@MacBookPro.lan>
2025-05-01 13:54:30 +0530

    express server setup
    Signed-off-by: shreha didwania <shrehadidwania@MacBookPro.lan>

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("HI")
+const express = require("express");
+
+const app = express();
+
+app.use((req, res) => {
+    res.send("Yes, I'm listening")
+})
+
+app.listen(8000, () => {
+    console.log("app is running")
+})
