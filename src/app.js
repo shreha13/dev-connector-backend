e764cd023bd2f69ab86ea01919b9f80cee590970
shreha didwania <shrehadidwania@MacBookPro.lan>
2025-05-03 13:38:18 +0530

    initial apis and validation
    Signed-off-by: shreha didwania <shrehadidwania@MacBookPro.lan>

--- src/app.js
+++ src/app.js
@@ -4,13 +4,25 @@ const { default: mongoose } = require("mongoose");
 const User = require("./models/user");
 
 const app = express();
+app.use(express.json());
+
+app.get("/users", (req, res, next) => {
+    User.find().then((users) => {
+        res.send(users)
+    })
+})
+app.patch("/users/:userId", (req, res, next) => {
+    const id = req.params.userId;
+    User.findByIdAndUpdate(id, req.body, { runValidators: true }).then((users) => {
+        res.send(users)
+    }).catch((err) => res.send("error: "+ err))
+})
 
 app.post("/signup", (req,res,next) => {
     const userObj = req.body;
-    console.log(req);
     console.log(userObj);
     const user = new User(userObj);
-    user.save().then(() => res.send("Saved successfully!")).catch(() => res.send("error"))
+    user.save().then(() => res.send("Saved successfully!")).catch((err) => res.send("error: "+ err))
 })
 
 connectDB().then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,63 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        maxLength: 12,
+        minLength: 3,
+        required: true
     },
     lastName: {
-        type: String
+        type: String,
+        required: true,
+        maxLength: 12,
+        minLength: 3
     },
     email: {
-        type: String
+        type: String,
+        required: true,
+        validate: [{
+            validator: function(v) {
+                const regex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/;
+
+                return regex.test(v);
+            },
+            message: "Not a correct email format"
+        }
+        // this is equal to unique
+        // {
+        //     validator: async function (value) {
+        //       // Check if email already exists
+        //       if (this.isNew || this.isModified('email')) {
+        //         const existing = await mongoose.models.User.findOne({ email: value });
+        //         return !existing;
+        //       }
+        //       return true;
+        //     },
+        //     message: 'Email already exists'
+        //   }
+        ], 
+        lowercase: true, 
+        trim: true,
+        required: true,
+        unique: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
         type: Number
     },
     gender: {
-        type: String
+        type: String,
+        enum: {
+            values: ["Female", "Male", "Others"]
+        }
     },
-});
+    skills: {
+        type: [String]
+    }
+}, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 
